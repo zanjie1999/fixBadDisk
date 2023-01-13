75ddbe07a1022d27f19dd7ec2d3745e483ccfce3
Sparkle <zanjie1999@yeah.net>
2023-01-13 11:44:41 +0800

    更准确的平均速度，支持单文件0.004M测试准确的4k读写，可以直接参数指定测速的写入总大小

--- fixBadDisk.py
+++ fixBadDisk.py
@@ -1,6 +1,6 @@
 # Sparkle 坏块屏蔽工具
 # 20200909
-# 3.1
+# 3.3
 
 fsize = 2
 doTest = True
@@ -21,11 +21,12 @@ def get_free_space_mb(folder):
 
 if len(argv) > 1:
     if argv[1] == '-h' or argv[1] == '--help':
-        print('Usage: fixBadDisk.py [filesize] [w|t]')
+        print('Usage: fixBadDisk.py [filesize] [w|t] [maxsize]')
         print('\t-h, --help: 显示帮助信息')
         print('\tfilesize: 单个文件大小，fat32下最大为4096M，且最多33000个文件')
         print('\tw: 只写入')
         print('\tt: 只测试')
+        print('\tmaxsize: 最大写入量，用于写入测速时指定大小')
         print('默认2M，写入后测试')
         exit()
     if argv[1] == 'w':
@@ -33,7 +34,7 @@ if len(argv) > 1:
     elif argv[1] == 't' or argv[1] == 'r':
         doWite = False
     else:
-        fsize = int(argv[1])
+        fsize = float(argv[1])
         
     if len(argv) > 2:
         if argv[2] == 'w':
@@ -51,40 +52,50 @@ os.chdir('bad')
 
 if doWite:
     print("\nWite...")
-    st = time.time()
+    allt = 0
     cn = 0
-    free = get_free_space_mb('.')
-    for i in range(0,int(free // fsize)):
-        print('\r' + str(i * fsize) + 'M/' + str(free) + 'M ' + str(round(cn * fsize / (time.time() - st), 3)) + 'M/s', end='')
-        b = os.urandom(1024 * 1024 * fsize)
+    if len(argv) > 3:
+        free = float(argv[3])
+    else:
+        free = get_free_space_mb('.')
+    for i in range(0, int(free // fsize)):
+        b = os.urandom(int(1024 * 1024 * fsize))
         n = hashlib.md5(b).hexdigest()[:8]
         try:
             with open(n,'wb') as f:
+                st = time.time()
                 f.write(b)
+                f.close()
+                allt += time.time() - st
         except Exception as e:
             os.remove(n)
             print(' except ' + n)
             print(e)
+        print('\r' + str(round(i * fsize, 3)) + 'M/' + str(free) + 'M ' + str(round(cn * fsize / allt, 3)) + 'M/s', end='     ')
         cn += 1
+        
 
 if doTest:
     print("\nTest...")
-    st = time.time()
+    allt = 0
     cn = 0
     files = os.listdir('.')
     allsize = len(files) * fsize
     for i, key in enumerate(files):
-        print('\r' + str(i * fsize) + 'M/' + str(allsize) + 'M ' + str(round(cn * fsize / (time.time() - st), 3)) + 'M/s', end='')
         try:
             with open(key,'rb') as f:
-                if hashlib.md5(f.read()).hexdigest()[:8] == key:
-                    f.close()
+                st = time.time()
+                d = f.read()
+                f.close()
+                allt += time.time() - st
+                if hashlib.md5(d).hexdigest()[:8] == key:
                     os.remove(key)
                 else:
                     print(' error ' + key)
         except Exception as e:
             print(' except ' + key)
             print(e)
+        print('\r' + str(round(i * fsize, 3)) + 'M/' + str(allsize) + 'M ' + str(round(cn * fsize / allt, 3)) + 'M/s', end='     ')
         cn += 1
 
 if os.path.exists('bad') and not os.listdir('bad'):
